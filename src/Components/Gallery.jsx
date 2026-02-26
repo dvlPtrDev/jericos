2291010db2a24de6a5cac40d44484ab97cf24c27
dvlPtrDev <davilapietro.sage@gmail.com>
2026-02-26 11:53:19 -0300

    Update Gallery.jsx

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,16 +1,14 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
+import logo from "../assets/logo-nobg.png";
 
 export default function Gallery() {
-  const images = [
-    "../assets/logo-nobg.png",
-    "../assets/logo-nobg.png",
-    "../assets/logo-nobg.png",
-    "../assets/logo-nobg.png",
-  ];
+  const images = [logo, logo, logo, logo];
 
   return (
-    <Container id="gallery" className="py-5" style={{marginTop: "auto"}}>
-      <h2 className="text-center mb-4" style={{color: "var(--red-dark)"}}>Galeria</h2>
+    <Container id="gallery" className="py-5" style={{ marginTop: "auto" }}>
+      <h2 className="text-center mb-4" style={{ color: "var(--red-dark)" }}>
+        Galeria
+      </h2>
       <Row className="g-4">
         {images.map((img, i) => (
           <Col xs={6} md={3} key={i}>
